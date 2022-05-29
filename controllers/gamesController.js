55c49e1e9663625ddeacad6a756d9de165de1bf1
unverzed <vitoriarortega@gmail.com>
2022-05-29 20:13:30 -0300

    feat: post games

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,29 +1,51 @@
 import db from "../database/db.js";
 
 export async function getGames(req, res) {
-    const { name } = req.query;
-    try {
-        const games = !name
-            ? await db.query(
-                  `SELECT games.*, 
+  const { name } = req.query;
+  try {
+    const games = !name
+      ? await db.query(
+          `SELECT games.*, 
                   categories.id as "categoryId", 
                   categories.name as "categoryName" 
                   FROM games 
                   JOIN categories 
                   ON games."categoryId" = categories.id;`
-              )
-            : await db.query(
-                  `SELECT games.*, 
+        )
+      : await db.query(
+          `SELECT games.*, 
                   categories.id as "categoryId", 
                   categories.name as "categoryName" 
                   FROM games 
                   JOIN categories 
                   ON games."categoryId" = categories.id 
                   WHERE LOWER(games.name) LIKE '${name.toLowerCase()}%';`
-              );
-        return res.send(games.rows);
-    } catch (e) {
-        console.log("deu erro ao fazer o get em games", e);
-        return res.sendStatus(500);
-    }
+        );
+    return res.send(games.rows);
+   
+  } catch (e) {
+    console.log("deu erro ao fazer o get em games", e);
+    return res.sendStatus(500);
+  }
+}
+
+export async function postGames(req, res) {
+  const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+  try {
+    await db.query(
+      `INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay")
+             VALUES ($1, $2, $3, $4, $5)`,
+      [
+        req.body.name,
+        req.body.image,
+        req.body.stockTotal,
+        req.body.categoryId,
+        req.body.pricePerDay,
+      ]
+    );
+    res.sendStatus(201);
+  } catch (e) {
+    console.log("erro ao criar o game", e);
+    return res.sendStatus(500);
+  }
 }
--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getGames } from "../controllers/gamesController.js";
+import { getGames, postGames } from "../controllers/gamesController.js";
 
 const gamesRouter = Router();
 
 gamesRouter.get("/games", getGames);
+gamesRouter.post("/games", postGames);
 
 export default gamesRouter;
