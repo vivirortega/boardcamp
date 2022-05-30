95e003fdb2af095f3082caa82f1bd5fdf6399cc4
unverzed <vitoriarortega@gmail.com>
2022-05-30 13:27:36 -0300

    feat: prepare for deploy

--- database/db.js
+++ database/db.js
@@ -5,8 +5,16 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const db = new Pool({
+const databaseConfig = {
     connectionString: process.env.DATABASE_URL,
-});
+};
+
+if(process.env.MODE === "PROD") {
+    databaseConfig.ssl = {
+        rejectUnauthorized: false
+    }
+}
+
+const db = new Pool(databaseConfig);
 
 export default db;
